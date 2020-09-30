e3d7a8e1e9632995c4165e2919bd9c1b37af77ae
facudelucia <facundodeluciajudo@gmail.com>
2020-09-30 17:59:37 -0300

    frases v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState, Fragment} from 'react';
+import soundMarcha from "./assets/audio/marcha-peronista.mp3"
 function App() {
+  const [frase, setFrase] = useState({})
+/*   
+  var sound = new Howl({
+        src: [soundMarcha]
+      }); sound.play()
+   */
+  const handleClick = async() => {
+    var audio = document.getElementById("player")
+    const api = await fetch("https://frases-peronchas.herokuapp.com/v1/quotes")
+    const frase = await api.json()
+    setFrase(frase[0])
+    if(audio.paused){
+      audio.play()
+
+    }else{
+      audio.pause()
+      audio.load()
+      audio.play()
+    }
+  }
+  const {author, quote} = frase
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Fragment>
+    <div className="slider">
+      
+      <div className="load">
+      </div>
+
+      <div className="content">
+
+        <div className="row flex-top justify-content-center">
+          <header className="header">
+            <h1>Frases peronchas</h1>
+            <p>Obtén frases de tus líderes peronistas favoritos</p>
+          </header>
+        </div>
+
+        <div className="row justify-content-center">
+          <div className="card text-center p-3">
+            <blockquote className="blockquote mb-0">
+              <p>"{quote}"</p>
+              <footer className=" text-right">
+                <p>- {author}</p>
+              </footer>
+            </blockquote>
+          </div>
+        </div>
+
+        <div className="row flex-top justify-content-center">
+          <button onClick={handleClick} id="button" className="btn btn-light btn-lg">
+          <audio id="player">
+            <source src={soundMarcha} type='audio/mpeg'/>
+          </audio>
+            Obtener frase
+          </button>
+        </div>
+
+      </div>
+
     </div>
+    <footer className="footer">
+      <p>Desarrollado por <a target="_blank" href="https://instagram.com/facudelucia"><i className="icon fab fa-instagram"></i></a> <a target="_blank" href="mailto:facundodeluciajudo@gmail.com"><i className="icon far fa-envelope"></i></a></p>
+    </footer>
+    </Fragment>
   );
 }
 
